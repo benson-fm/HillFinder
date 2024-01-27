20a3a8ea2d1959157bb266308efdb2241b3e50b2
LexTruong <lextruong777@gmail.com>
2024-01-26 23:46:48 -0800

    Implemented distance.js and got distance and time from 1 location to another.

---
+++ distance.js
@@ -0,0 +1,25 @@
+function get_distance() {
+    const service = new google.maps.DistanceMatrixService();
+    // build request
+    const origin1 = "UC Irvine";
+    const destinationA = "10510 Moledo Ct Elk Grove";
+    const request = {
+        origins: [origin1],
+        destinations: [destinationA],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.IMPERIAL,
+        avoidHighways: false,
+        avoidTolls: false,
+    };
+
+    // get distance matrix response
+    service.getDistanceMatrix(request).then((response) => {
+        // process response
+        distanceMiles = response["rows"][0]["elements"][0]["distance"]["text"]
+        time = response["rows"][0]["elements"][0]["duration"]["text"]
+        console.log(distanceMiles)
+        console.log(time)
+    })
+}
+
+get_distance()
